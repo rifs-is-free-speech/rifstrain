eeea888e6febc3a084b947fb091244a4776cde6c
Anders Stendevad <as@flowtale.ai>
2023-05-02 13:59:24 +0200

    Updated callbacks

--- src/rifstrain/callbacks.py
+++ src/rifstrain/callbacks.py
@@ -6,9 +6,10 @@ File containing callback functions used during Training.
 We implement the following Callback options:
 
     - CsvLogger
-    - Timekeeper 
+    - StatusUpdater
+    - Timekeeper
 
-These are used to log training progress and make sure we 
+These are used to log training progress and make sure we
 gracefully stop models when they get close to the time limit.
 """
 
@@ -22,7 +23,7 @@ class CsvLogger(TrainerCallback):
     """
 
     def __init__(self):
-        """ Constructor for CsvLogger 
+        """Constructor for CsvLogger
 
         Parameters
         ----------
@@ -32,6 +33,17 @@ class CsvLogger(TrainerCallback):
         pass
 
     def on_init_end(self, args, state, control, **kwargs):
+        """Callback function for logging training and evaluation metrics to a csv file.
+
+        On trainer initialization will set up the csv file.
+
+        Parameters
+        ----------
+        args
+        state
+        control
+        kwargs
+        """
         json_data = {
             "run_id": self.run_id,
             "start_time": pendulum.now().to_iso8601_string(),
@@ -41,13 +53,70 @@ class CsvLogger(TrainerCallback):
         }
 
         now = pendulum.now(tz="Europe/Paris").format("HH:mm:ss DD-MM-YYYY")
-        
+
+    def on_step_begin(self, args, state, control, **kwargs):
+        """Callback function for logging training and evaluation metrics to a csv file.
+
+        On step on training step begin will write to CSV previous metrics if any
+
+        Parameters
+        ----------
+        args
+        state
+        control
+        kwargs
+        """
+        pass
+
+
+class StatusUpdater(TrainerCallback):
+    """
+    Small callback used to track status and metrics related to performance
+    """
+
+    def __init__(self):
+        """Constructor for StatusUpdater
+
+        Parameters
+        ----------
+        None
+
+        """
+        pass
+
+    def on_init_end(self, args, state, control, **kwargs):
+        """Called at the end of Trainer initialization
+
+        Used to report when the Trainer has been initialized.
+
+        Parameters
+        ----------
+        args
+        state
+        control
+        kwargs
+        """
+        pass
+
+        now = pendulum.now(tz="Europe/Paris").format("HH:mm:ss DD-MM-YYYY")
+
         json_data = {
             "run_id": self.run_id,
             "status": f"Init done - {now}",
         }
 
     def on_step_begin(self, args, state, control, **kwargs):
+        """Called at the beginning of each training step
+
+        Will report the current step and status.
+
+        Parameters
+        ----------
+        args
+        state
+        control
+        kwargs
+        """
         json_data = {
             "run_id": self.run_id,
             "status": f"Running: Step {state.global_step} - {now}",
@@ -60,8 +129,8 @@ class Timekeeper(TrainerCallback):
     """
 
     def __init__(self, hours, minutes, run_id, token):
-        """
-        Constructor for Timekeeper
+        """Constructor for Timekeeper
+
         Parameters
         ----------
         hours: int
